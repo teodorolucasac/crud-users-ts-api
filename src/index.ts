16056cc5dbcaa0df079176693fa80ff5d34e6f55
Lucas Teodoro <lucasteodoroac@gmail.com>
2023-04-03 13:40:39 -0300

    feat: add MongoCreateUserRepository

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,29 @@
-import express from 'express'
-import { config } from 'dotenv'
-import { GetUsersController } from './controllers/get-users/get-users';
-import { MongoGetUsersRepository } from './repositories/get-users/mongo-get-users';
-import { MongoClient } from './database/mongo';
+import express from "express";
+import { config } from "dotenv";
+import { GetUsersController } from "./controllers/get-users/get-users";
+import { MongoGetUsersRepository } from "./repositories/get-users/mongo-get-users";
+import { MongoClient } from "./database/mongo";
 
 const main = async () => {
-  config()
-  
-  const app = express()
+  config();
 
-  await MongoClient.connect()
+  const app = express();
 
-  app.get('/users', async (req, res) => {
+  await MongoClient.connect();
+
+  app.get("/users", async (req, res) => {
     const mongoGetUsersRepository = new MongoGetUsersRepository();
-    
+
     const getUsersController = new GetUsersController(mongoGetUsersRepository);
-  
-    const { body, statusCode} = await getUsersController.handle();
-  
+
+    const { body, statusCode } = await getUsersController.handle();
+
     res.send(body).status(statusCode);
-  })
+  });
 
   const port = process.env.PORT || 8000;
 
   app.listen(port, () => console.log(`listening on port ${port}!`));
-}
+};
 
 main();
